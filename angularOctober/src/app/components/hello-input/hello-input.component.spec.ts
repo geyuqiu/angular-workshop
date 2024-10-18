687879f55e6f2829ab15f175d23b346502014ed7
Yuqiu Ge <yuqiu.ge@gmail.com>
2024-10-18 14:01:45 +0200

    add unit test for nameInput

--- angularOctober/src/app/components/hello-input/hello-input.component.spec.ts
+++ angularOctober/src/app/components/hello-input/hello-input.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HelloInputComponent } from './hello-input.component';
+import { By } from '@angular/platform-browser';
+import { NameInputComponent } from '../name-input/name-input.component';
 
 describe('HelloInputComponent', () => {
   let component: HelloInputComponent;
@@ -19,5 +21,31 @@ describe('HelloInputComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('show hello at beginning', () => {
+    const paragraph = fixture.debugElement.query(By.css('p')).nativeElement;
+    expect(paragraph.innerText).toContain('Hello');
+  });
+
+  it('show hello after triggering handelNewName', () => {
+    component.handleNewName('Max');
+    fixture.detectChanges();
+
+    const paragraph = fixture.debugElement.query(By.css('p')).nativeElement;
+
+    expect(paragraph.innerText).toContain('Hello Max');
+  });
+
+  it('show hello after triggering by output', () => {
+    const nameInput = fixture.debugElement
+      .query(By.directive(NameInputComponent))
+      .componentInstance;
+    nameInput.nameSubmitted.emit('Max');
+
+    fixture.detectChanges();
+
+    const paragraph = fixture.debugElement.query(By.css('p')).nativeElement;
+    expect(paragraph.innerText).toContain('Hello Max');
   });
 });
