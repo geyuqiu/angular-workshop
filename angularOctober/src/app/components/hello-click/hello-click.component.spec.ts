fd2f847a2415bcc7373cafc9eb3abccfc5ff1a4d
Yuqiu Ge <yuqiu.ge@gmail.com>
2024-10-18 13:51:34 +0200

    add unit test for hello click

--- angularOctober/src/app/components/hello-click/hello-click.component.spec.ts
+++ angularOctober/src/app/components/hello-click/hello-click.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HelloClickComponent } from './hello-click.component';
+import { By } from '@angular/platform-browser';
 
 describe('HelloClickComponent', () => {
   let component: HelloClickComponent;
@@ -19,5 +20,20 @@ describe('HelloClickComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('trigger click', () => {
+    expect(component.buttonWasClicked).toBeFalsy();
+    component.sayHello();
+    expect(component.buttonWasClicked).toBeTruthy();
+  });
+
+  it('emulate click', () => {
+    expect(component.buttonWasClicked).toBeFalsy();
+    fixture.debugElement.query(By.css('button')).nativeElement.click();
+    expect(component.buttonWasClicked).toBeTruthy();
+    fixture.detectChanges();
+    const paragraphText = fixture.debugElement.query(By.css('p')).nativeElement.innerText;
+    expect(paragraphText).toEqual('Hello World');
   });
 });
