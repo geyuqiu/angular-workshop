30d1d7b3bcf12be583412408c00667a8a9584488
Yuqiu Ge <yuqiu.ge@gmail.com>
2024-10-18 13:44:34 +0200

    add unit test for hello list (ngfor)

--- angularOctober/src/app/components/hello-list/hello-list.component.spec.ts
+++ angularOctober/src/app/components/hello-list/hello-list.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HelloListComponent } from './hello-list.component';
+import { By } from '@angular/platform-browser';
 
 describe('HelloListComponent', () => {
   let component: HelloListComponent;
@@ -19,5 +20,18 @@ describe('HelloListComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  it('should show greetings for 3 persons', () => {
+    const paragraphDebugElements = fixture
+      .debugElement.queryAll(By.css('p'));
+
+    expect(paragraphDebugElements.length).toEqual(3);
+    expect(paragraphDebugElements[0].nativeElement.innerText)
+      .toEqual('hallo Manuela');
+    expect(paragraphDebugElements[1].nativeElement.innerText)
+      .toEqual('hallo Sebastian');
+    expect(paragraphDebugElements[2].nativeElement.innerText)
+      .toEqual('hallo Abiram');
+
   });
 });
