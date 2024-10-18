dda20ba1a509dc5af0358c2c73fc45d898880bc5
Yuqiu Ge <yuqiu.ge@gmail.com>
2024-10-18 10:05:11 +0200

    ngif einbinden

--- angularOctober/src/app/components/hello/hello.component.ts
+++ angularOctober/src/app/components/hello/hello.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-hello',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './hello.component.html',
   styleUrl: './hello.component.scss'
 })
 export class HelloComponent {
-
+  @Input() name: string = '';
 }
